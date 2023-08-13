6d2d3452035684bc5c708c0a5eba5f71366d449c
Alex <78169857+msunay@users.noreply.github.com>
2023-08-13 12:13:52 +0100

    mediasoup install

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,11 @@
 "use client";
 import styles from "./page.module.css";
+import * as mediasoup from "mediasoup";
+import { types as mediasoupTypes } from "mediasoup";
 
 export default function Home() {
+
+  
   return (
     <main></main>
   );
