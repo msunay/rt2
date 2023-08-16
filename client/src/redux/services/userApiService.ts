cf707de739298817aa5b0d4967df026ef02e36e7
Alex <78169857+msunay@users.noreply.github.com>
2023-08-16 22:29:10 +0100

    fix: use .env variable for backend url

--- client/src/redux/services/userApiService.ts
+++ client/src/redux/services/userApiService.ts
@@ -23,7 +23,7 @@ interface UserPost {
   password: string
 }
 
-const BASE_URL = 'http://localhost:3001/'
+const BASE_URL = process.env.BASE_URL || 'http://localhost:3001/'
 
 export const userApiService = {
   postUser: async function (user: UserPost): Promise<ResponseUser> {
