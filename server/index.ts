3804e9dc27f20780e7e1ce7dc8d760f4c8425985
Alex <78169857+msunay@users.noreply.github.com>
2023-08-16 22:42:08 +0100

    security: cors options set

--- server/index.ts
+++ server/index.ts
@@ -4,7 +4,11 @@ import router from "./router";
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: process.env.NODE_ENV === "production" ? process.env.CORS_ORIGIN : 'http://localhost:3000',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(router);
 
