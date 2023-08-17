692afaf53d7aeac76501f3f7b12d523443ed2d00
ismaiilov-atai <ataismaiilov@gmail.com>
2023-08-18 01:02:53 +0600

    merge: develop to ui-dashboard

--- server/controllers/referenceTables.controller.ts
+++ server/controllers/referenceTables.controller.ts
@@ -1,5 +1,6 @@
 import models from "../models/index";
 import { Request, Response } from "express";
+import { CustomRequest } from "../middleware/auth";
 
 async function createParticipation(req: Request, res: Response) {
   try {
@@ -11,6 +12,21 @@ async function createParticipation(req: Request, res: Response) {
     res.status(201).send(participationInstance);
   } catch (err) {
     console.error("Could not create participation::", err);
+    res.status(500).send();
+  }
+}
+
+
+async function getUserParticipations(req: Request, res: Response) {
+  try {
+    const response = await models.Participation.findAll({
+      where: {
+        UserId: (req as CustomRequest).userId,
+      },
+    });
+    res.status(200).send(response);
+  } catch (err) {
+    console.error("Could not get participations::", err);
     res.status(500).send();
   }
 }
@@ -33,4 +49,5 @@ async function createParticipationAnswer(req: Request, res: Response) {
 export default {
   createParticipation,
   createParticipationAnswer,
+  getUserParticipations
 };
