8fe8a616a252fc9e5bf5abad6262566359bd001f
Thiago <thiagolos1999@gmail.com>
2023-08-22 12:31:08 +0100

    create user and fetch by id routes test passing

--- server/__tests__/index.test.js
+++ server/__tests__/index.test.js
@@ -1,36 +1,50 @@
-import supertest from 'supertest';
-import models from '../models/index';
+import { initModels } from '../models/associations';
 import { Sequelize } from 'sequelize';
+import mocks from '../utils/mocks';
+import supertest from 'supertest';
+import dotenv from 'dotenv';
 import app from '../index';
-import { beforeEach } from 'mocha';
+
+dotenv.config();
+const env = process.env;
 
 const localTestConnection = [
   `${env.TEST_DB_NAME}`,
   `${env.TEST_DB_USERNAME}`,
   `${env.TEST_DB_PASSWORD}`,
   {
-    host: "localhost",
+    host: 'localhost',
 
-    dialect: "postgres",
+    dialect: 'postgres',
 
     logging: false,
-  }
+  },
 ];
 
 const sequelize = new Sequelize(...localTestConnection);
+const models = initModels(sequelize);
 
 describe('User endpoint tests', () => {
   const request = supertest(app);
 
   beforeAll(async () => {
-    await sequelize.sync().then(() => console.log('Connected to test database'););
-  })
+    await sequelize
+      .sync()
+      .then(() => console.log('Connected to test database'));
+  });
 
-  beforeEach(async () => {
-    await models.User.truncate();
-  })
+  afterEach(async () => {
+    await models.User.destroy({ where: {} });
+  });
 
-  it("should add a user", async () => {
-    
-  })
+  it('should add a user and fetch it by id', async () => {
+    let response = await request.get('/users');
+    expect(JSON.parse(response.text).length).toBe(0);
+
+    await models.User.create(mocks.players[0]);
+
+    response = await request.get(`/userDetails/${mocks.players[0].id}`);
+    console.log('RESPONSE TEXT::', JSON.parse(response.text));
+    expect(JSON.parse(response.text).id).toBe(mocks.players[0].id);
+  });
 });
--- server/utils/populateDatabase.ts
+++ server/utils/populateDatabase.ts
@@ -1,5 +1,6 @@
 import models from '../models/index';
 import moment from 'moment';
+import mocks from './mocks';
 /*
   The following function populates the database with 5 users (1 host and 4 players) and 4 quizzes.
   The host user will be the owner of all 4 quizzes and the players are generated without any participations.
@@ -12,59 +13,6 @@ import moment from 'moment';
   4. Run the command: "ts-node controllers/populateDatabase.ts"
   5. You will see the log "Database populated" -> this means it was successful.
 */
-const mocks = {
-  hosts: [
-    {
-      id: '0cf7cbe4-0c52-4a1a-940a-65d47f769c08',
-      email: 'mock@host.com',
-      username: 'jimmyHost',
-      password: '$2a$10$72VY7l.IIX49MmRSGNj8aebXRXyDyWkZyRU8DKl4gchKam410AJyK',
-      isPremiumMember: true,
-      pointsWon: 0,
-    },
-  ],
-  players: [
-    {
-      id: 'efde9495-1ec4-4c7d-b344-b68000c00291',
-      email: 'dave@email.com',
-      username: 'dave',
-      password: '$2a$10$72VY7l.IIX49MmRSGNj8aebXRXyDyWkZyRU8DKl4gchKam410AJyK',
-      isPremiumMember: false,
-      pointsWon: 0,
-    },
-    {
-      id: '67136c1f-573e-4f57-a403-91719b98584e',
-      email: 'mike@email.com',
-      username: 'mike',
-      password: '$2a$10$72VY7l.IIX49MmRSGNj8aebXRXyDyWkZyRU8DKl4gchKam410AJyK',
-      isPremiumMember: false,
-      pointsWon: 0,
-    },
-    {
-      id: '78b9e627-c22b-495e-973f-5aa0bd95a525',
-      email: 'john@email.com',
-      username: 'john',
-      password: '$2a$10$72VY7l.IIX49MmRSGNj8aebXRXyDyWkZyRU8DKl4gchKam410AJyK',
-      isPremiumMember: false,
-      pointsWon: 0,
-    },
-    {
-      id: '87363573-d48f-43e9-81a0-3d64af38380b',
-      email: 'kevin@email.com',
-      username: 'kevin',
-      password: '$2a$10$72VY7l.IIX49MmRSGNj8aebXRXyDyWkZyRU8DKl4gchKam410AJyK',
-      isPremiumMember: false,
-      pointsWon: 0,
-    },
-  ],
-  quizIdArray: [
-    '98e03864-eec4-4800-941c-4b1dbe78301f',
-    'fb5a9d14-f083-4ec3-9d7c-05b920a250fd',
-    '7e3ad734-fd07-45d4-acf9-3d12b05bcb19',
-    'd0da9fc4-d186-4186-bf32-d0bb2fad1f8f',
-    '690c7980-336d-4ee6-ab92-8edb832b174d',
-  ],
-};
 
 async function addMockUsers() {
   try {
