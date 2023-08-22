d590307a3fdf7f805e7f9d365819d5aa1dca4864
Carlos Bárcena Bescansa <carlos.barcena.bescansa@gmail.com>
2023-08-22 15:28:11 +0200

    Added styles to Play Quiz button and directed QuizLoading to user stream when countdown 0

--- client/src/components/QuizLoadingComponents/CountdownTimer.tsx
+++ client/src/components/QuizLoadingComponents/CountdownTimer.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import styles from '../../app/QuizLoading/quizLoading.module.css';
+import router from 'next/router';
 
 interface CountdownTimerProps {
   startTime: string;
@@ -30,14 +31,20 @@ const CountdownTimer: React.FC<CountdownTimerProps> = ({ startTime }) => {
       const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
       setTimeRemaining({ days, hours, minutes, seconds });
+
+      if (distance <= 0) {
+        //TODO: Update the route
+        router.push('/UserStream/');
+      }
     }, 1000);
 
     return () => clearInterval(interval);
-  }, [startTime]);
+  }, [startTime, router]);
 
   const renderTime = () => {
     if (timeRemaining.days > 0) {
-      return `${timeRemaining.days} days ${String(timeRemaining.hours).padStart(2, '0')}:${String(
+      const dayText = timeRemaining.days === 1 ? 'day' : 'days';
+      return `${timeRemaining.days} ${dayText} ${String(timeRemaining.hours).padStart(2, '0')}:${String(
         timeRemaining.minutes
       ).padStart(2, '0')}:${String(timeRemaining.seconds).padStart(2, '0')}`;
     }
