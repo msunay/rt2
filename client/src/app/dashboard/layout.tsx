0d4023e681ee56ed32edeb039d391bd800c3006a
ismaiilov-atai <ataismaiilov@gmail.com>
2023-08-22 14:57:27 +0600

    feat: logout button

--- client/src/app/dashboard/layout.tsx
+++ client/src/app/dashboard/layout.tsx
@@ -3,6 +3,10 @@ import '../globals.css';
 import { Providers } from '@/redux/provider';
 import styles from '@/styles/auth.module.css';
 import { useRouter } from 'next/navigation';
+import exit_icon from '@/public/icons-exit.png';
+import Image from 'next/image';
+import { useDispatch } from 'react-redux';
+import { setAuthState } from '@/redux/features/authSlice';
 
 export default function RootLayout({
   children,
@@ -10,9 +14,27 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   const router = useRouter();
+  const dispatch = useDispatch();
+  
+  function exitUser () {
+    localStorage.clear();
+    dispatch(setAuthState(''));
+    router.push('/')
+  }
+
   return (
     <div className={styles.auth_container}>
-      <nav onClick={() => router.push('/dashboard')}>Real Time Trivia</nav>
+      <nav onClick={() => router.push('/dashboard')}>
+        <p> Real Time Trivia</p>
+        <Image
+          className='exit-icon'
+          src={exit_icon}
+          width={35}
+          height={30}
+          alt='exit image'
+          onClick={() => exitUser()}
+        />
+        </nav>
       <Providers>{children}</Providers>
     </div>
   );
