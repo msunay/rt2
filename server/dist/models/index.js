e9e56438256a7252d8bb8c9d549fba311d6f9875
Alex <78169857+msunay@users.noreply.github.com>
2023-09-21 03:43:00 +0100

    github workflow

--- server/dist/models/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const associations_1 = require("./associations");
-const db_1 = __importDefault(require("../db"));
-const models = (0, associations_1.initModels)(db_1.default);
-exports.default = models;
