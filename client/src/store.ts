04b8b97baf8ea5f02e025c363da42e4c34416235
Alex <78169857+msunay@users.noreply.github.com>
2024-06-14 14:04:17 +0100

    test workflow

--- client/src/store.ts
+++ client/src/store.ts
@@ -43,7 +43,7 @@ export const store: EnhancedStore = configureStore({
 
 setupListeners(store.dispatch);
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
+// Infer the `RootState` and `AppDispatch` types from the store itself.
 export type RootState = ReturnType<typeof rootReducer>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
